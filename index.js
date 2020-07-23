c4ca218e69f6c9a94908b3184bd46f9a1fa928b5
Matthew <mattk.development@gmail.com>
2020-07-23 14:19:37 -0500

    Task 1 Complete

--- index.js
+++ index.js
@@ -29,20 +29,39 @@ class Airplane {
 
 /*
   TASK 1
-    - Write a Person class whose constructor initializes `name` and `age` from arguments.
-    - All instances of Person should also initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
-        + When eating an edible, it should be pushed into the `stomach`.
-        + The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
-        + When an instance poops, its `stomach` should empty.
-    - Give instances of Person a method `.toString()`:
-        + It should return a string with `name` and `age`. Example: "Mary, 50"
+    - X Write a Person class whose constructor initializes `name` and `age` from arguments.
+    -  X All instances of Person should also initialize with an empty `stomach` array.
+    - X Give instances of Person the ability to `.eat("someFood")`:
+        X + When eating an edible, it should be pushed into the `stomach`.
+        X + The `eat` method should have no effect if there are 10 items in the `stomach`.
+    - X Give instances of Person the ability to `.poop()`:
+        X + When an instance poops, its `stomach` should empty.
+    - X Give instances of Person a method `.toString()`:
+        X + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(edible){
+    if(this.stomach.length < 10){
+      return this.stomach.push(edible);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name} and ${this.age}`;
+  }
 }
+
+const personOne = new Person("Matt", 30);
+
+personOne.eat("Pizza");
 
 /*
   TASK 2
@@ -59,6 +78,7 @@ class Person {
 */
 
 class Car {
+  constructor(attributes)
 
 }
 
